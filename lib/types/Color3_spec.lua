6dbc56e590444147336bbdc0fdce7843e6637417
boyned <boynedmaster@gmail.com>
2017-12-13 20:55:55 -0800

    Add Color3.new empty constructor and Color3.fromRGB

--- lib/types/Color3_spec.lua
+++ lib/types/Color3_spec.lua
@@ -1,9 +1,29 @@
 local Color3 = import("./Color3")
 
+local function extractColors(color)
+	return {
+		color.r,
+		color.g,
+		color.b
+	}
+end
+
 describe("types.Color3", function()
-	it("should have a constructor", function()
+	it("should have an empty constructor", function()
+		local color = Color3.new()
+
+		assert.not_nil(color)
+	end)
+
+	it("should have a constructor that takes rgb values 0-1", function()
 		local color = Color3.new(0, 0, 0)
 
 		assert.not_nil(color)
+	end)
+
+	it("should have the fromRGB method", function()
+		local color = Color3.fromRGB(255, 0, 0)
+
+		assert.are.same(extractColors(color), { 1, 0, 0 })
 	end)
 end)
