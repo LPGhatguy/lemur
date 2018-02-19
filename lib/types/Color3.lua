e4c3f1361cbccc6477e7066af650ade8c9aac954
boyned//Kampfkarren <boynedmaster@gmail.com>
2018-02-18 16:00:33 -0800

    Add Color3.new empty constructor and Color3.fromRGB (#2)

--- lib/types/Color3.lua
+++ lib/types/Color3.lua
@@ -1,11 +1,21 @@
 local Color3 = {}
 
-function Color3.new(r, g, b)
-	return {
-		r = r,
-		g = g,
-		b = b,
-	}
+function Color3.new(...)
+	if select("#", ...) == 0 then
+		return Color3.new(0, 0, 0)
+	else
+		local r, g, b = ...
+
+		return {
+			r = r,
+			g = g,
+			b = b,
+		}
+	end
+end
+
+function Color3.fromRGB(r, g, b)
+	return Color3.new(r / 255, g / 255, b / 255)
 end
 
 return Color3
