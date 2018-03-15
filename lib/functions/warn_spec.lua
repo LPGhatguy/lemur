326632c91f0f4907a307dd0e7a07fe3914561cc4
Kampfkarren <boynedmaster@gmail.com>
2018-03-15 21:14:22 +0000

    Fix whitespace (when will I learn)

--- lib/functions/warn_spec.lua
+++ lib/functions/warn_spec.lua
@@ -4,20 +4,20 @@ describe("functions.warn", function()
 	it("should be a function", function()
 		assert.is_function(warn)
 	end)
-	
+
 	it("should warn", function()
 	   local oldErr = io.stderr
-	   
+
 	   local writeSpy = spy.new(function(_, msg) end)
-	   
+
 	   io.stderr = {
 	       write = writeSpy
 	   }
-	   
+
 	   warn("Doge has taken over the world!")
-	   
+
 	   assert.spy(writeSpy).was_called_with(io.stderr, "Doge has taken over the world!")
-	   
+
 	   io.stderr = oldErr
 	end)
 end)
