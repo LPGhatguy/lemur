f29f13a28f9598f5cda84902b7b1de6437270d62
boyned//Kampfkarren <boynedmaster@gmail.com>
2018-03-16 11:45:57 -0700

    Add more coverage (#12)

--- lib/instances/TestService_spec.lua
+++ lib/instances/TestService_spec.lua
@@ -7,4 +7,21 @@ describe("instances.TestService", function()
 		assert.not_nil(instance)
 		assert.not_nil(instance.Error)
 	end)
+
+	it("should write to stderr", function()
+		local instance = Instance.new("TestService")
+		local oldErr = io.stderr
+
+		local writeSpy = spy.new(function(_, msg) end)
+
+		io.stderr = { -- luacheck: ignore
+			write = writeSpy
+		}
+
+		instance:Error("Testing tests in a library for testing?")
+
+		assert.spy(writeSpy).was_called_with(io.stderr, "Testing tests in a library for testing?")
+
+		io.stderr = oldErr -- luacheck: ignore
+	end)
 end)
