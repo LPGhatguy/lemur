44a45861b243093430eb4c45e263edeed2ddb19b
Kampfkarren <boynedmaster@gmail.com>
2018-03-16 16:21:23 -0700

    More coverage

--- lib/functions/warn_spec.lua
+++ lib/functions/warn_spec.lua
@@ -1,23 +1,33 @@
 local warn = import("./warn")
 
+local function setupWarnDetour()
+	local oldErr = io.stderr
+
+	local writeSpy = spy.new(function(_, msg) end)
+
+	io.stderr = { -- luacheck: ignore
+		write = writeSpy
+	}
+
+	return oldErr, writeSpy
+end
+
 describe("functions.warn", function()
 	it("should be a function", function()
 		assert.is_function(warn)
 	end)
 
 	it("should warn", function()
-		local oldErr = io.stderr
-
-		local writeSpy = spy.new(function(_, msg) end)
-
-		io.stderr = { -- luacheck: ignore
-			write = writeSpy
-		}
-
+		local oldErr, writeSpy = setupWarnDetour()
 		warn("Doge has taken over the world!")
-
 		assert.spy(writeSpy).was_called_with(io.stderr, "Doge has taken over the world!")
+		io.stderr = oldErr -- luacheck: ignore
+	end)
 
+	it("should warn with multiple arguments", function()
+		local oldErr, writeSpy = setupWarnDetour()
+		warn("IT'S DOGE!", "AAH!")
+		assert.spy(writeSpy).was.called(3) -- IT'S DOGE, \t, AAH!
 		io.stderr = oldErr -- luacheck: ignore
 	end)
 end)
