f8e152f27322a67843bb83e26d7ed8045cadabb4
boyned//Kampfkarren <boynedmaster@gmail.com>
2018-03-22 09:38:02 -0700

    Spaces -> Tabs

--- lib/fs_spec.lua
+++ lib/fs_spec.lua
@@ -1,24 +1,24 @@
 describe("fs", function()
-  describe("fs", function()
-    it("should error when lfs is not installed", function()
-      --To successfully test this, I must do the grossest hack.
-      local oldRequire = require
+	describe("fs", function()
+		it("should error when lfs is not installed", function()
+			--To successfully test this, I must do the grossest hack.
+			local oldRequire = require
 
-      _G.require = function(name)
-        if name == "lfs" then
-          error()
-        else
-          return oldRequire(name)
-        end
-      end
+			_G.require = function(name)
+				if name == "lfs" then
+					error()
+				else
+					return oldRequire(name)
+				end
+			end
 
-      assert.has.errors(function()
-        import("./fs")
-      end)
+			assert.has.errors(function()
+				import("./fs")
+			end)
 
-      _G.require = oldRequire
-    end)
-  end)
+			_G.require = oldRequire
+		end)
+	end)
 
 	local fs = import("./fs")
 
