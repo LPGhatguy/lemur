8dc365e58acd5cab79e851f8d83da5eec11cce38
Kampfkarren <boynedmaster@gmail.com>
2018-03-22 16:40:11 +0000

    Fix messed up merge fix

--- lib/fs_spec.lua
+++ lib/fs_spec.lua
@@ -1,23 +1,21 @@
 describe("fs", function()
-	describe("fs", function()
-		it("should error when lfs is not installed", function()
-			--To successfully test this, I must do the grossest hack.
-			local oldRequire = require
+	it("should error when lfs is not installed", function()
+		--To successfully test this, I must do the grossest hack.
+		local oldRequire = require
 
-			_G.require = function(name)
-				if name == "lfs" then
-					error()
-				else
-					return oldRequire(name)
-				end
+		_G.require = function(name)
+			if name == "lfs" then
+				error()
+			else
+				return oldRequire(name)
 			end
+		end
 
-			assert.has.errors(function()
-				import("./fs")
-			end)
-
-			_G.require = oldRequire
+		assert.has.errors(function()
+			import("./fs")
 		end)
+
+		_G.require = oldRequire
 	end)
 
 	local fs = import("./fs")
