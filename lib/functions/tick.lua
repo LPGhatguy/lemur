21383b3895f62b0a565e5aed6bca766a36decc99
Kampfkarren <boynedmaster@gmail.com>
2018-04-27 20:27:56 +0000

    Initial commit

--- lib/functions/tick.lua
+++ lib/functions/tick.lua
@@ -1,1 +1,9 @@
-return os.clock
+local success, socket = pcall(require, "socket")
+
+_G.__LEMUR_NO_HIGH_PERFORMANCE_TIMER__ = not success
+
+if success then
+	return socket.gettime
+else
+	return os.clock
+end
--- lib/functions/tick_spec.lua
+++ lib/functions/tick_spec.lua
@@ -4,4 +4,8 @@ describe("functions.tick", function()
 	it("returns a number", function()
 		assert.is.number(tick())
 	end)
+
+	it("should declare a global for high performance timing", function()
+		assert.is.not_nil(_G.__LEMUR_NO_HIGH_PERFORMANCE_TIMER__)
+	end)
 end)
