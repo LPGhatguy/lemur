27454a119ae194f3c03f3b8d60262c94d2bed0e1
Kampfkarren <boynedmaster@gmail.com>
2018-04-28 02:15:43 +0000

    Remove global

--- lib/functions/tick.lua
+++ lib/functions/tick.lua
@@ -1,7 +1,5 @@
 local success, socket = pcall(require, "socket")
 
-_G.__LEMUR_NO_HIGH_PERFORMANCE_TIMER__ = not success
-
 if success then
 	return socket.gettime
 else
--- lib/functions/tick_spec.lua
+++ lib/functions/tick_spec.lua
@@ -4,8 +4,4 @@ describe("functions.tick", function()
 	it("returns a number", function()
 		assert.is.number(tick())
 	end)
-
-	it("should declare a global for high performance timing", function()
-		assert.is.not_nil(_G.__LEMUR_NO_HIGH_PERFORMANCE_TIMER__)
-	end)
 end)
