5d93820adcd553273a976a5e2fd520ee34fe63fd
Andrew Portner <aportner@gmail.com>
2018-05-04 15:18:03 -0700

    added ScreenGui.DisplayOrder

--- lib/instances/ScreenGui_spec.lua
+++ lib/instances/ScreenGui_spec.lua
@@ -6,4 +6,24 @@ describe("instances.ScreenGui", function()
 
 		assert.not_nil(instance)
 	end)
+
+	describe("DisplayOrder", function()
+		it("should have a value that is a number", function()
+			local instance = Instance.new("ScreenGui")
+			assert.equals(type(instance.DisplayOrder), "number")
+		end)
+
+		it("should be settable", function()
+			local instance = Instance.new("ScreenGui")
+			instance.DisplayOrder = 2
+			assert.equals(instance.DisplayOrder, 2)
+		end)
+
+		it("should only accept numbers", function()
+			local instance = Instance.new("ScreenGui")
+			assert.has.errors(function()
+				instance.DisplayOrder = 2
+			end)
+		end)
+	end)
 end)
