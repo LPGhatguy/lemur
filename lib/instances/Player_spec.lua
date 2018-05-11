ac0a04fed063d14390815b48683b733a7706c7c8
Andrew Portner <aportner@gmail.com>
2018-05-11 11:55:06 -0700

    added Player.UserId (#65)

--- lib/instances/Player_spec.lua
+++ lib/instances/Player_spec.lua
@@ -5,5 +5,18 @@ describe("instances.Player", function()
 		local instance = Player:new()
 
 		assert.not_nil(instance)
+		assert.equals(instance.UserId, 0)
+	end)
+
+	it("should take a userId", function()
+		local instance = Player:new(1234)
+
+		assert.equals(instance.UserId, 1234)
+	end)
+
+	it("should throw when userId is not a number", function()
+		assert.has.errors(function()
+			Player:new("1234")
+		end)
 	end)
 end)
