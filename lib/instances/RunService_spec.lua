6c1e5c480d472a39e40fcc554ead71bd3dfec2f5
Andrew Portner <aportner@gmail.com>
2018-05-11 10:43:53 -0700

    added RenderStepped to RunService (#63)

--- lib/instances/RunService_spec.lua
+++ lib/instances/RunService_spec.lua
@@ -1,20 +1,28 @@
-local Instance = import("../Instance")
+local RunService = import("./RunService")
+local Signal = import("../Signal")
 
 describe("instances.RunService", function()
 	it("should instantiate", function()
-		local instance = Instance.new("RunService")
+		local instance = RunService:new()
 
 		assert.not_nil(instance)
 		assert.not_nil(instance.Heartbeat)
 	end)
 
 	it("should have the name 'Run Service'", function()
-		local instance = Instance.new("RunService")
+		local instance = RunService:new()
 		assert.is_equal(instance.Name, "Run Service")
 	end)
 
 	it("should return false when IsStudio() is called", function()
-		local instance = Instance.new("RunService")
+		local instance = RunService:new()
 		assert.is_equal(instance.IsStudio(), false)
+	end)
+
+	it("should have properties defined", function()
+		local instance = RunService:new()
+
+		assert.equals(instance.Heartbeat.__index, Signal)
+		assert.equals(instance.RenderStepped.__index, Signal)
 	end)
 end)
