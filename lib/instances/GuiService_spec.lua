f665acd56653a3ea7059a95de4e67403af70d8e6
Andrew Portner <aportner@gmail.com>
2018-06-18 11:06:43 -0700

    added more things to gui service (#77)

--- lib/instances/GuiService_spec.lua
+++ lib/instances/GuiService_spec.lua
@@ -1,9 +1,33 @@
-local Instance = import("../Instance")
+local GuiService = import("./GuiService")
 
 describe("instances.GuiService", function()
 	it("should instantiate", function()
-		local instance = Instance.new("GuiService")
+		local instance = GuiService:new()
 
 		assert.not_nil(instance)
+	end)
+
+	describe("BroadcastNotification", function()
+		it("should exist", function()
+			GuiService:new().BroadcastNotification("test", 123)
+		end)
+	end)
+
+	describe("GetNotificationTypeList", function()
+		it("should return a table", function()
+			assert.equal(type(GuiService:new().GetNotificationTypeList()), "table")
+		end)
+	end)
+
+	describe("SetGlobalGuiInset", function()
+		it("should exist", function()
+			GuiService:new().SetGlobalGuiInset(1, 2, 3, 4)
+		end)
+	end)
+
+	describe("SafeZoneOffsetsChanged", function()
+		it("should exist", function()
+			GuiService:new().SafeZoneOffsetsChanged()
+		end)
 	end)
 end)
