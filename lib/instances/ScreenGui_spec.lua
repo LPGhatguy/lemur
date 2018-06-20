1484ec8cafac5078e0956beee62b82a364d72202
Andrew Portner <aportner@gmail.com>
2018-06-20 16:18:04 -0700

    fixed unit test

--- lib/instances/ScreenGui_spec.lua
+++ lib/instances/ScreenGui_spec.lua
@@ -1,32 +1,32 @@
-local ScreenGui = import("./ScreenGui")
+local Instance = import("../Instance")
 local typeof = import("../functions/typeof")
 
 describe("instances.ScreenGui", function()
 	it("should instantiate", function()
-		local instance = ScreenGui:new()
+		local instance = Instance.new("ScreenGui")
 
 		assert.not_nil(instance)
 	end)
 
 	it("should have properties defined", function()
-		local instance = ScreenGui:new()
+		local instance = Instance.new("ScreenGui")
 		assert.equals(typeof(instance.ZIndexBehavior), "number")
 	end)
 
 	describe("DisplayOrder", function()
 		it("should have a value that is a number", function()
-			local instance = ScreenGui:new()
+			local instance = Instance.new("ScreenGui")
 			assert.equals(type(instance.DisplayOrder), "number")
 		end)
 
 		it("should be settable", function()
-			local instance = ScreenGui:new()
+			local instance = Instance.new("ScreenGui")
 			instance.DisplayOrder = 2
 			assert.equals(instance.DisplayOrder, 2)
 		end)
 
 		it("should only accept numbers", function()
-			local instance = ScreenGui:new()
+			local instance = Instance.new("ScreenGui")
 			assert.has.errors(function()
 				instance.DisplayOrder = "string"
 			end)
@@ -35,7 +35,7 @@ describe("instances.ScreenGui", function()
 
 	describe("AbsoluteSize", function()
 		it("should return a Vector2", function()
-			local instance = ScreenGui:new()
+			local instance = Instance.new("ScreenGui")
 			assert.equals(typeof(instance.AbsoluteSize), "Vector2")
 		end)
 	end)
