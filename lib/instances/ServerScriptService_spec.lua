59baab01671ff9f5b13d09174a473850d9a81cf5
Kampfkarren <boynedmaster@gmail.com>
2018-08-13 12:41:19 -0700

    Fix whitespace

--- lib/instances/ServerScriptService_spec.lua
+++ lib/instances/ServerScriptService_spec.lua
@@ -4,7 +4,7 @@ local lemur = require("lib")
 describe("instances.ServerScriptService", function()
 	it("should instantiate", function()
 		local instance = ServerScriptService:new()
-		
+
 		assert.not_nil(instance)
 	end)
 
