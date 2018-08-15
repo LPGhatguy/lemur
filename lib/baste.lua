e0fbc2581454024f160bfd2f84553c7fe74b980b
Lucien Greathouse <me@lpghatguy.com>
2018-08-14 17:14:12 -0700

    Upgrade baste

--- lib/baste.lua
+++ lib/baste.lua
@@ -29,57 +29,147 @@
 
 local baste = {}
 
-local path = {}
-baste._path = path
+local function componentsFromPathString(input)
+	local components = {}
+	local sliceStart = 1
+	local sliceEnd = 0
 
---[[
-	Normalize the given path by removing unnecessary slashes, `.`, and `..`.
-]]
-function path.normalize(input)
-	return (input
-		-- Normalize \ and / to /
-		:gsub("[/\\]+", "/")
-		-- Remove trailing slashes
-		:gsub("/+$", "")
-		-- Handle .
-		:gsub("^%./", "")
-		:gsub("/%.$", "/")
-		:gsub("/%./", "/")
-		-- Handle ..
-		:gsub("[^/]+/%.%./", ""))
+	for i = 1, #input do
+		local char = input:sub(i, i)
+
+		if char == "/" or char == "\\" then
+			if sliceEnd ~= 0 then
+				local slice = input:sub(sliceStart, sliceEnd)
+
+				if slice == ".." then
+					local lastComponent = components[#components]
+
+					if lastComponent ~= nil and lastComponent ~= ".." then
+						table.remove(components)
+					else
+						table.insert(components, slice)
+					end
+				elseif slice ~= "." then
+					table.insert(components, slice)
+				end
+			end
+
+			sliceStart = i + 1
+			sliceEnd = 0
+		else
+			sliceEnd = i
+		end
+	end
+
+	if sliceEnd ~= 0 then
+		local slice = input:sub(sliceStart, sliceEnd)
+		table.insert(components, slice)
+	end
+
+	return components
 end
 
---[[
-	Returns everything except the last node in the path. This will be the folder
-	containing the input path.
+local Path = {}
+Path.prototype = {}
+Path.__index = Path.prototype
 
-	When used in combination with path.leaf and path.join, it's possible to
-	reconstruct a path with the same semantic meaning as the original input:
+function Path.fromString(input)
+	if type(input) ~= "string" then
+		error("Path.fromString expects a string, but got " .. type(input), 2)
+	end
 
-	`input` vs `path.join(path.withoutLeaf(input), path.leaf(input))`
-]]
-function path.withoutLeaf(input)
-	input = path.normalize(input)
+	local isAbsolute = input:sub(1, 1) == "/"
 
-	return (input:match("^(.+)/[^/]+$")) or "."
+	local path = {
+		isAbsolute = isAbsolute,
+		components = componentsFromPathString(input),
+	}
+
+	setmetatable(path, Path)
+
+	return path
 end
 
---[[
-	Get the file extension of the given path, or nil if there isn't one.
-]]
-function path.extension(input)
-	return (input:match("%.([^./]-)$"))
+function Path:__tostring()
+	if self.__stringRepresentation ~= nil then
+		return self.__stringRepresentation
+	end
+
+	local output = table.concat(self.components, "/")
+
+	if self.isAbsolute then
+		output = "/" .. output
+	end
+
+	self.__stringRepresentation = output
+
+	return output
 end
 
---[[
-	Joins together the given list of path fragments.
+function Path.prototype:getExtension()
+	local lastComponent = self.components[#self.components]
 
-	Only the first path fragment is allowed to be an absolute path, but this
-	isn't verified.
-]]
-function path.join(...)
-	return path.normalize(table.concat({...}, "/"))
+	if lastComponent == nil then
+		return nil
+	end
+
+	-- TODO: Handle files that start with a dot?
+
+	return lastComponent:match("%.[^.]+")
 end
+
+function Path.prototype:clone()
+	local components = {}
+
+	for _, component in ipairs(self.components) do
+		table.insert(components, component)
+	end
+
+	local newPath = {
+		isAbsolute = self.isAbsolute,
+		components = components,
+	}
+
+	setmetatable(newPath, getmetatable(self))
+
+	return newPath
+end
+
+function Path.prototype:push(input)
+	self = self:clone()
+
+	local newComponents = componentsFromPathString(input)
+
+	for _, component in ipairs(newComponents) do
+		if component == ".." then
+			if #self.components > 0 then
+				table.remove(self.components)
+			else
+				table.insert(self.components, component)
+			end
+		elseif component ~= "." then
+			table.insert(self.components, component)
+		end
+	end
+
+	return self
+end
+
+function Path.prototype:pop()
+	self = self:clone()
+	table.remove(self.components)
+
+	return self
+end
+
+function Path.prototype:addExtension(extension)
+	self = self:clone()
+	self.components[#self.components] = self.components[#self.components] .. extension
+
+	return self
+end
+
+baste._Path = Path
 
 -- Abstraction over loadstring and load
 local loadWithEnv
@@ -145,9 +235,13 @@ local moduleResults = {}
 	file's path into the generated function. This also reduces the number of
 	debug library calls.
 ]]
-local function makeImport(root)
+local function makeImport(rootPath)
 	return function(modulePath)
-		local current = root or debug.getinfo(2, "S").source:gsub("^@", "")
+		local currentPath = rootPath
+
+		if currentPath == nil then
+			currentPath = Path.fromString(debug.getinfo(2, "S").source:gsub("^@", ""))
+		end
 
 		if type(modulePath) ~= "string" then
 			local message = "Bad argument #1 to import, expected string but got %s"
@@ -156,31 +250,31 @@ local function makeImport(root)
 
 		-- Relative import!
 		if modulePath:sub(1, 1) == "." then
-			local currentDirectory = path.withoutLeaf(current)
-			local relativeModulePath = path.join(currentDirectory, modulePath)
+			local currentDirectory = currentPath:pop()
+			local relativeModulePath = currentDirectory:push(modulePath)
 
 			local pathsToTry = {relativeModulePath}
 
-			if not path.extension(modulePath) then
-				table.insert(pathsToTry, relativeModulePath .. ".lua")
-				table.insert(pathsToTry, path.join(relativeModulePath, "init.lua"))
+			if Path.fromString(modulePath):getExtension() == nil then
+				table.insert(pathsToTry, relativeModulePath:addExtension(".lua"))
+				table.insert(pathsToTry, relativeModulePath:push("init.lua"))
 			end
 
 			-- TODO: Plug-in point for adding additional paths to try
 
 			-- Have we loaded this module before?
-			for _, target in ipairs(pathsToTry) do
-				if loadedModules[target] then
-					return moduleResults[target]
+			for _, path in ipairs(pathsToTry) do
+				if loadedModules[tostring(path)] then
+					return moduleResults[tostring(path)]
 				end
 			end
 
 			-- Let's try to load from these paths!
-			for _, target in ipairs(pathsToTry) do
+			for _, path in ipairs(pathsToTry) do
 				-- Hand-craft an environment for the module we're loading
 				-- The module won't be able to iterate over globals!
 				local env = setmetatable({
-					import = makeImport(target),
+					import = makeImport(path),
 				}, {
 					__index = _G,
 					__newindex = _G,
@@ -188,15 +282,15 @@ local function makeImport(root)
 
 				-- TODO: Plug-in point for adding extra loaders
 
-				local source = readFile(target)
+				local source = readFile(tostring(path))
 
 				if source then
-					local chunk, err = loadWithEnv(source, "@" .. target, env)
+					local chunk, err = loadWithEnv(source, "@" .. tostring(path), env)
 
 					if chunk then
 						local result = chunk()
-						loadedModules[target] = true
-						moduleResults[target] = result
+						loadedModules[tostring(path)] = true
+						moduleResults[tostring(path)] = result
 
 						return result
 					else
@@ -206,11 +300,17 @@ local function makeImport(root)
 				end
 			end
 
+			local pathsToTryAsStrings = {}
+
+			for _, path in ipairs(pathsToTry) do
+				table.insert(pathsToTryAsStrings, tostring(path))
+			end
+
 			-- We didn't find any modules.
 			local message = string.format("Couldn't import %q from file %s, tried:\n\t%s",
 				modulePath,
-				current,
-				table.concat(pathsToTry, "\n\t")
+				tostring(currentPath),
+				table.concat(pathsToTryAsStrings, "\n\t")
 			)
 
 			error(message, 2)
