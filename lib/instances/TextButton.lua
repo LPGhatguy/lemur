9c82c2ce14b12d40a061dffeaee4a05730dd501f
jeparlefrancais <jeparlefrancais21@gmail.com>
2018-08-20 15:03:30 -0700

    Add properties to TextButton
    Add TextWrapped, TextXAlignment and TextYAlignment

--- lib/instances/TextButton.lua
+++ lib/instances/TextButton.lua
@@ -1,4 +1,5 @@
 local Color3 = import("../types/Color3")
+local Enum = import("../Enum")
 local Font = import("../Enum/Font")
 local GuiButton = import("./GuiButton")
 local InstanceProperty = import("../InstanceProperty")
@@ -28,6 +29,24 @@ TextButton.properties.TextColor3 = InstanceProperty.typed("Color3", {
 TextButton.properties.TextSize = InstanceProperty.typed("number", {
 	getDefault = function()
 		return 14
+	end,
+})
+
+TextButton.properties.TextWrapped = InstanceProperty.typed("boolean", {
+	getDefault = function()
+		return false
+	end,
+})
+
+TextButton.properties.TextXAlignment = InstanceProperty.typed("number", {
+	getDefault = function()
+		return Enum.TextXAlignment.Left
+	end,
+})
+
+TextButton.properties.TextYAlignment = InstanceProperty.typed("number", {
+	getDefault = function()
+		return Enum.TextYAlignment.Top
 	end,
 })
 
