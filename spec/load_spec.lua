d26d0632c2f07c73f5fdbe57989a65302838d670
Lucien Greathouse <me@lpghatguy.com>
2018-10-15 13:18:44 -0700

    Load non-ModuleScript scripts from disk

--- spec/load_spec.lua
+++ spec/load_spec.lua
@@ -7,30 +7,41 @@ describe("Lemur", function()
 
 		local root = habitat:loadFromFs("spec/test-project")
 
-		assert.equal(#root:GetChildren(), 3)
+		assert.equal(#root:GetChildren(), 6)
 
 		local bar = root:FindFirstChild("bar")
-		local usurp = root:FindFirstChild("usurp")
-		local normal = root:FindFirstChild("normal-folder")
-
 		assert.equal(bar.ClassName, "ModuleScript")
-		assert.equal(usurp.ClassName, "ModuleScript")
-		assert.equal(normal.ClassName, "Folder")
-
 		assert.equal(bar.Source, "-- bar.lua")
-		assert.equal(usurp.Source, "-- init.lua")
 
+		local usurp = root:FindFirstChild("usurp")
 		assert.equal(#usurp:GetChildren(), 1)
+		assert.equal(usurp.ClassName, "ModuleScript")
+		assert.equal(usurp.Source, "-- init.lua")
 
 		local foo = usurp:FindFirstChild("foo")
 		assert.equal(foo.ClassName, "ModuleScript")
 		assert.equal(foo.Source, "-- foo.lua")
 
+		local normal = root:FindFirstChild("normal-folder")
 		assert.equal(#normal:GetChildren(), 1)
+		assert.equal(normal.ClassName, "Folder")
 
 		local ack = normal:FindFirstChild("ack")
 		assert.equal(ack.ClassName, "ModuleScript")
 		assert.equal(ack.Source, "-- ack.lua")
+
+		local abc = root:FindFirstChild("abc")
+		assert.equal(abc.ClassName, "Script")
+		assert.equal(abc.Source, "-- abc.server.lua")
+
+		local def = root:FindFirstChild("def")
+		assert.equal(def.ClassName, "LocalScript")
+		assert.equal(def.Source, "-- def.client.lua")
+
+		local usurpServer = root:FindFirstChild("usurp-server")
+		assert.equal(#usurpServer:GetChildren(), 0)
+		assert.equal(usurpServer.ClassName, "Script")
+		assert.equal(usurpServer.Source, "-- usurp-server/init.server.lua")
 	end)
 
 	it("should not touch init.lua if loadInitModules is false", function()
@@ -40,7 +51,7 @@ describe("Lemur", function()
 			loadInitModules = false,
 		})
 
-		assert.equal(#root:GetChildren(), 3)
+		assert.equal(#root:GetChildren(), 6)
 
 		local usurp = root:FindFirstChild("usurp")
 		assert.equal(usurp.ClassName, "Folder")
@@ -52,5 +63,17 @@ describe("Lemur", function()
 		local foo = usurp:FindFirstChild("foo")
 		assert.equal(foo.ClassName, "ModuleScript")
 		assert.equal(foo.Source, "-- foo.lua")
+
+		local abc = root:FindFirstChild("abc")
+		assert.equal(abc.ClassName, "Script")
+		assert.equal(abc.Source, "-- abc.server.lua")
+
+		local def = root:FindFirstChild("def")
+		assert.equal(def.ClassName, "LocalScript")
+		assert.equal(def.Source, "-- def.client.lua")
+
+		local usurpServer = root:FindFirstChild("usurp-server")
+		assert.equal(#usurpServer:GetChildren(), 1)
+		assert.equal(usurpServer.ClassName, "Folder")
 	end)
 end)
