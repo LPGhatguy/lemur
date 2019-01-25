78bfe22d12c358677b24d86a70439962de2b2b0a
Kampfkarren <boynedmaster@gmail.com>
2019-01-25 08:16:48 -0800

    Add comment explaining why

--- lib/functions/pcall.lua
+++ lib/functions/pcall.lua
@@ -1,3 +1,5 @@
+-- pcall in Lua 5.1 cannot yield, this is a patch for that
+
 return function(f, ...)
 	return coroutine.resume(coroutine.create(function(...)
 		-- coroutine.create gets mad if it's not a *real* Lua function
