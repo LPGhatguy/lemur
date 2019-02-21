e5111bb42d42696962e83d0654b9b656e6f19e30
froghopperjacob <froghopperjacob@gmail.com>
2019-02-20 21:05:47 -0500

    Add NumberRange (#155)

---
+++ lib/types/NumberRange_spec.lua
@@ -0,0 +1,23 @@
+local NumberRange = import("./NumberRange")
+local typeof = import("../functions/typeof")
+
+describe("types.NumberRange", function()
+	it("should have a constructor that takes values 0-Infitity", function()
+		local range = NumberRange.new(1, 2)
+
+		assert.not_nil(range)
+	end)
+
+	it("should compare NumberRanges", function()
+		local nr1, nr2 = NumberRange.new(1, 2), NumberRange.new(1, 2)
+
+		assert.are.equal(nr1, nr2)
+		assert.are_not_equal(nr1, NumberRange.new(1, 3))
+	end)
+
+	it("should be detected by typeof", function()
+		local type = typeof(NumberRange.new())
+
+		assert.are.equal("NumberRange", type)
+	end)
+end)
