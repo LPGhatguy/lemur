33fba7a681f87f56e30fe32abc9274238a10365e
froghopperjacob <froghopperjacob@gmail.com>
2019-02-20 21:15:09 -0500

    Forogt single argument check, formatting

--- lib/types/NumberRange_spec.lua
+++ lib/types/NumberRange_spec.lua
@@ -1,11 +1,26 @@
 local NumberRange = import("./NumberRange")
 local typeof = import("../functions/typeof")
 
+local function extractRange(range)
+	return {
+		range.Min,
+		range.Max
+	}
+end
+
 describe("types.NumberRange", function()
 	it("should have a constructor that takes values 0-Infitity", function()
 		local range = NumberRange.new(1, 2)
 
 		assert.not_nil(range)
+		assert.are.equal(extractRange(range), { 1, 2 })
+	end)
+
+	it("should have a constructor that takes one value", function()
+		local range = NumberRange.new(1)
+
+		assert.not_nil(rang)
+		assert.are.equal(extractRange(range), { 1, 1 })
 	end)
 
 	it("should compare NumberRanges", function()
